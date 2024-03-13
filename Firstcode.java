1c0f16b176a2b10184982c9ce9433626083de8d1
mramineni <raminenimaheshbabu13@gmail.com>
2024-03-13 13:52:48 -0500

    This is my second code for commit

--- Firstcode.java
+++ Firstcode.java
@@ -1,6 +1,6 @@
 public class Mahesh{
     public static void main(static[] args){
-        String str="hello this is mahesh";
+        String str="hello this is nani";
         system.out.println(str);
     }
 }
